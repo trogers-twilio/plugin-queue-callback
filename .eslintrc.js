04fa0bb914bccb181b3150e23037cd70392c2e47
Terence Rogers <trogers@twilio.com>
2019-07-31 10:40:03 -0700

    Defining ESLint rules

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb",
+    parser: "babel-eslint",
+    rules: {
+        "import/no-extraneous-dependencies": 0,
+        "comma-dangle": 0,
+        "func-names": 0,
+        "space-before-function-paren": 0,
+        "arrow-parens": 0,
+        "no-console": 0,
+        "consistent-return": 0,
+        "class-methods-use-this": 0,
+        "quotes": ["error", "single", { avoidEscape: true }]
+    }
+};
